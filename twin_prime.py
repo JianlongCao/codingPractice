43118111cabe0868a65b5005f7062311a6534af2
Jianlong Cao <cao.jianlong@siren.care>
2019-05-16 23:10:40 +0800

    add twin

---
+++ twin_prime.py
@@ -0,0 +1,31 @@
+import math
+def isPrime(n):
+    """check if is prime number"""
+    # Corner cases
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+
+    # This is checked so that we
+    # can skip middle five numbers
+    # in below loop
+    if n%2 == 0 or n%3 == 0:
+        return False
+
+    for i in range(5, int(math.sqrt(n)+1), 6):
+        if n%i == 0 or n%(i + 2) == 0:
+            return False
+
+    return True
+
+j = 6;
+pair_num = 1;
+target = 10**5
+while j < target:
+    if isPrime(j-1) and isPrime(j+1):
+        #print("prime is " + str(j-1) + ", " + str(j+1))
+        pair_num = pair_num + 1;
+    j = j + 6;
+
+print("total " + str(pair_num))
